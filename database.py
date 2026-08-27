40bda523cfb63286a6500c65a5a9355f40a1ca6f
donghwanoh97 <odh9568@gmail.com>
2026-08-27 13:09:05 +0900

    requirements 추가

--- database.py
+++ database.py
@@ -1,5 +1,8 @@
 import os
 from pymongo import MongoClient
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # 환경변수가 없을 경우 기본 로컬 MongoDB 사용
 MONGO_URI = os.getenv('MONGO_URI', 'mongodb://localhost:27017/')
